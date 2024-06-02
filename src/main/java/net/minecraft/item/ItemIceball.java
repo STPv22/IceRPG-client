55096d77bf5b2a1a68f291e9bce4ee6099a51bb8
stpv22 <167645243+stpv221@users.noreply.github.com>
2024-06-01 18:29:19 -0700

    add snowball temperarily for teting

--- src/main/java/net/minecraft/item/ItemIceball.java
+++ src/main/java/net/minecraft/item/ItemIceball.java
@@ -2,7 +2,8 @@ package net.minecraft.item;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.monster.EntityBlaze;
+import net.minecraft.entity.projectile.EntityIceball;
+import net.minecraft.entity.projectile.EntitySnowball;
 import net.minecraft.stats.StatList;
 import net.minecraft.world.World;
 
@@ -43,7 +44,7 @@ public class ItemIceball extends Item {
 
 		world.playSoundAtEntity(entityplayer, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
 		if (!world.isRemote) {
-			world.spawnEntityInWorld(new EntityBlaze(world, entityplayer));
+			world.spawnEntityInWorld(new EntitySnowball(world, entityplayer));
 		}
 
 		entityplayer.triggerAchievement(StatList.objectUseStats[Item.getIdFromItem(this)]);
